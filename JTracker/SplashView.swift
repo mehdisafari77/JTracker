9713d44fa629475ee205cf86b8090c7c93d850fe
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-05-19 18:38:13 -0700

    add splash screen

---
+++ JTracker/SplashView.swift
@@ -0,0 +1,38 @@
+//
+//  SplashView.swift
+//  JTracker
+//
+//  Created by Mehdi MMS on 19/05/2022.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    @State var isActive:Bool = false
+    
+    var body: some View {
+        VStack {
+            if self.isActive {
+                HomeView()
+            } else {
+                Image("splashLogo")
+                    .resizable()
+                    .frame(width: 150, height: 150, alignment: .center)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
