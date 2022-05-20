d30236c1f92bc93d1e3f5889aa4097424222f6b7
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-05-19 18:18:56 -0700

    Initial Commit

---
+++ JTracker/JTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JTrackerApp.swift
+//  JTracker
+//
+//  Created by Mehdi MMS on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct JTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
